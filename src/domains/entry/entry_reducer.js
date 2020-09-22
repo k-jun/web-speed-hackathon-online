5932d052f418a231cf9df6fc3c9d69e0c0fc52e6
keijun.kumagai <keijun091221@gmail.com>
2020-09-22 16:00:21 +0900

    feat(modules): remove immutable

--- src/domains/entry/entry_reducer.js
+++ src/domains/entry/entry_reducer.js
@@ -1,14 +1,15 @@
-import { fromJS, Map } from 'immutable';
 import { ACTION_ENTRY_FETCHED, ACTION_LIKE_UPDATED } from './entry_actions';
 
-export function entryReducer(state = Map(), action) {
+export function entryReducer(state = {}, action) {
   switch (action.type) {
     case ACTION_ENTRY_FETCHED: {
-      return fromJS(action.data.entry);
+      // return fromJS();
+      return action.data.entry;
     }
 
     case ACTION_LIKE_UPDATED: {
-      return state.set('like_count', action.data.likeCount);
+      // return state.set('like_count', action.data.likeCount);
+      return { like_count: action.data.likeCount };
     }
 
     default: {
