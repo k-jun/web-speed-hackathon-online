0f8f1339c216149aefa11c36e78b59991416e080
keijun.kumagai <keijun091221@gmail.com>
2020-09-22 17:49:23 +0900

    feat(asset): retrive image from js file

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpe?g|gif)$/,
         use: {
-          loader: 'url-loader',
+          loader: 'file-loader',
         },
       },
     ],
